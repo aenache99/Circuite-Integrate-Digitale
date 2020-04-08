df45d6034d00923cad3bd2dc13f4628fab8be5d0
Alex Enache <55555550+punmeistervstheworld@users.noreply.github.com>
2020-04-08 21:48:37 +0300

    Add files via upload

---
+++ CID - Lab Test 1 - 201X/bitwise_sum.v
@@ -0,0 +1,5 @@
+module bitwise_sum(input wire [3:0] in0,
+						 input wire [3:0] in1,
+						 output wire [3:0]out);
+assign out = in0 ^ in1;
+endmodule 
---
+++ CID - Lab Test 1 - 201X/sumator.v
@@ -0,0 +1,6 @@
+module sumator(input [2:0] in0,
+					input [2:0] in1,
+					output [3:0] out
+);
+assign out=in0+in1;
+endmodule 
