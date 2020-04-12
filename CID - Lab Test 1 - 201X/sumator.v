cd2877b0833c518cd52f78ae2b9c8c5b98f1e91c
Alex Enache <55555550+punmeistervstheworld@users.noreply.github.com>
2020-04-12 21:39:14 +0300

    sumator.v

--- CID - Lab Test 1 - 201X/sumator.v
+++ CID - Lab Test 1 - 201X/sumator.v
@@ -1,6 +1,6 @@
-module sumator(input [2:0] in0,
-					input [2:0] in1,
-					output [3:0] out
+module sumator(input [1:0] in0,
+					input [1:0] in1,
+					output [2:0] out
 );
 assign out=in0+in1;
 endmodule 
