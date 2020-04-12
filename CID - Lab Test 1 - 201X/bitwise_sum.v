bae833c147462f7bdd45122429552535148f2021
Alex Enache <55555550+punmeistervstheworld@users.noreply.github.com>
2020-04-12 21:47:34 +0300

    Update bitwise_sum.v

--- CID - Lab Test 1 - 201X/bitwise_sum.v
+++ CID - Lab Test 1 - 201X/bitwise_sum.v
@@ -1,5 +1,5 @@
-module bitwise_sum(input wire [3:0] in0,
-						 input wire [3:0] in1,
-						 output wire [3:0]out);
+module bitwise_sum(input wire [2:0] in0,
+		   input wire [2:0] in1,
+		   output wire [2:0]out);
 assign out = in0 ^ in1;
 endmodule 
